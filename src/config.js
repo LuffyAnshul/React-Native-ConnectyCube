0076934acec18898aa66578cc4c66a4522d8f51a
LuffyAnshul <anshulborawake@gmail.com>
2021-01-08 11:08:17 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    appId: 4019,
+    authKey: 'chGF3eNjx8yswgb',
+    authSecret: '8ZpjbxvRwcuPcKQ',
+  },
+  {
+    debug: {mode: 1},
+  },
+];
+
+export const users = [
+  {
+    id: 3220033,
+    name: 'Anshul',
+    login: 'anshul',
+    password: 'anshul123456',
+    color: '#34ad86',
+  },
+  {
+    id: 3220038,
+    name: 'Saurabh',
+    login: 'saurabh',
+    password: 'saurabh123456',
+    color: '#077988',
+  },
+  {
+    id: 3220040,
+    name: 'Ajay',
+    login: 'ajay123456',
+    password: 'ajay123456',
+    color: '#13aaae',
+  },
+  {
+    id: 590583,
+    name: 'Pankaj',
+    login: 'pankaj',
+    password: 'pankaj123456',
+    color: '#056a96',
+  },
+];
